23deb218ef0c210037c1707539c375aa70791921
artalfred <bernalesartalfred.com>
2024-12-02 10:21:31 +0800

    Fixed form layout

--- app/components/contactForm.tsx
+++ app/components/contactForm.tsx
@@ -13,78 +13,80 @@ function ContactForm() {
   }
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="grid gap-[1rem] my-[2rem]">
-          <div className="flex gap-[2rem] w-full">
-            <div className="grid gap-2 w-full">
-              <label htmlFor="name">Name</label>
+      <div className="bg-white lg:p-[2rem] md:p-[1.5rem] sm:p-[1rem] p-[1rem] shadow-md rounded-xl">
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-[1rem]">
+            <div className="grid lg:flex md:flex sm:grid gap-[2rem] w-full">
+              <div className="grid gap-2 w-full">
+                <label htmlFor="name">Name</label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  className="border rounded-xl px-4 py-2 outline-none lg:w-[15rem] md:w-[12rem] sm:w-[25rem] w-[20rem] form-size"
+                />
+                <ValidationError
+                  prefix="name"
+                  field="name"
+                  errors={state.errors}
+                />
+              </div>
+
+              <div className="grid gap-2 w-full">
+                <label htmlFor="last-name">Last Name</label>
+                <input
+                  id="last-name"
+                  type="text"
+                  name="lastName"
+                  className="border rounded-xl px-4 py-2 outline-none lg:w-[15rem] md:w-[12rem] sm:w-[25rem] w-[20rem] form-size"
+                />
+                <ValidationError
+                  prefix="Last Name"
+                  field="lastName"
+                  errors={state.errors}
+                />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <label htmlFor="email">Email Address</label>
               <input
-                id="name"
-                type="text"
-                name="name"
+                id="email"
+                type="email"
+                name="email"
                 className="border rounded-xl px-4 py-2 outline-none"
               />
               <ValidationError
-                prefix="name"
-                field="name"
+                prefix="Email"
+                field="email"
                 errors={state.errors}
               />
             </div>
 
-            <div className="grid gap-2 w-full">
-              <label htmlFor="last-name">Last Name</label>
-              <input
-                id="last-name"
-                type="text"
-                name="lastName"
-                className="border rounded-xl px-4 py-2 outline-none"
+            <div className="grid gap-2">
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                className="border rounded-xl outline-none p-4"
+                rows={5}
               />
               <ValidationError
-                prefix="Last Name"
-                field="lastName"
+                prefix="Message"
+                field="message"
                 errors={state.errors}
               />
             </div>
-          </div>
-          <div className="grid gap-2">
-            <label htmlFor="email">Email Address</label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              className="border rounded-xl px-4 py-2 outline-none"
-            />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={state.errors}
-            />
-          </div>
 
-          <div className="grid gap-2">
-            <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              name="message"
-              className="border rounded-xl outline-none p-4"
-              rows={5}
-            />
-            <ValidationError
-              prefix="Message"
-              field="message"
-              errors={state.errors}
-            />
+            <button
+              className="dark-button p-3 rounded-xl mt-3"
+              type="submit"
+              disabled={state.submitting}
+            >
+              Submit
+            </button>
           </div>
-
-          <button
-            className="dark-button p-3 rounded-xl"
-            type="submit"
-            disabled={state.submitting}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import ContactForm from "../components/contactForm";
 import Container from "../components/container";
+import MainContainer from "../components/mainContainer";
 import Title from "../components/title";
 
 const Contact = () => {
@@ -11,27 +12,30 @@ const Contact = () => {
           <section className="px-[1rem] rounded-2xl py-[6rem] flex items-center justify-center">
             <div>
               <div className="grid gap-[1rem]">
-                <div className="grid justify-start">
+                <div className="grid justify-center">
                   <Title>
                     <span className="green">Chat Now</span>
                   </Title>
                 </div>
 
-                <div className="flex justify-start">
-                  <p className="green lg:w-[70%] md:w-[80%] sm:w-[100%] w-[100%]">
+                <div className="flex justify-center">
+                  <p className="green text-center lg:w-[70%] md:w-[100%] sm:w-[100%] w-[100%]">
                     Explore a curated collection of insightful articles designed
                     to inform, inspire, and empower. From expert tips to
                     in-depth guides, our content covers a range of topics to
                     keep you informed and ahead of the curve.
                   </p>
                 </div>
-                <div className="lg:w-[40rem] md:w-[30rem] sm:w-[100%] w-full">
-                  <ContactForm />
-                </div>
               </div>
             </div>
           </section>
         </Container>
+
+        <MainContainer>
+          <div className="flex justify-center w-full pb-[3rem] mt-[-2rem]">
+            <ContactForm />
+          </div>
+        </MainContainer>
       </div>
     </>
   );
