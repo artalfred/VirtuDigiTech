5939cf9fe85710340874709716888b8ee4778bbf
artalfred <bernalesartalfred.com>
2024-12-03 15:01:21 +0800

    New Dropdown Update for Solutions Navigation

--- app/components/navMenu.tsx
+++ app/components/navMenu.tsx
@@ -61,6 +61,7 @@ export function Solutions() {
                 return (
                   <Link
                     href="/contact"
+                    key={list.title}
                     className={cn(
                       "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                     )}
